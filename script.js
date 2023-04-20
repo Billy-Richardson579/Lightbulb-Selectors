22810a64192deea4d562555cc1db83975a6ab36e
Billy Richardson <billy7373731@outlook.com>
2023-04-20 12:02:53 +0100

    bulbs Selectors

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// Write your code here
+const bulb1 = document.querySelector("lightbulb1")
+const bulb2 = document.querySelector("lightbulb2")
+const bulb3 = document.querySelector("lightbulb3")
+let count = 0
+
