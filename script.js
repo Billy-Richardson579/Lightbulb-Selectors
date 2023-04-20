78f9c0d380b85ff10292eb715170d1c568b0a160
Billy Richardson <billy7373731@outlook.com>
2023-04-20 12:46:57 +0100

    working

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const bulb1 = document.querySelector("lightbulb1")
-const bulb2 = document.querySelector("lightbulb2")
-const bulb3 = document.querySelector("lightbulb3")
+const bulb1 = document.querySelector("#lightbulb1")
+const bulb2 = document.querySelector("#lightbulb2")
+const bulb3 = document.querySelector("#lightbulb3")
+const subtitle = document.querySelector('.subtitle');
 let count = 0
+
+bulb1.addEventListener('click', function(){
+    count++;
+    subtitle.innerHTML = count; 
+    this.classList.toggle("active");
+})
+
+bulb2.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = count; 
+    this.classList.toggle("active");
+})
+bulb3.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = count; 
+    this.classList.toggle("active");
+})
+
 
